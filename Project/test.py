47e43b54924cb3e3c262baaa13866eae00f6e981
yjk54 <146088525+yjk54@users.noreply.github.com>
2023-11-30 20:47:37 +0900

    tmp

---
+++ Project/test.py
@@ -0,0 +1,10 @@
+def testfunction(name, colour_id, gender):
+    counter = 0
+    for i in range(10):
+        print(i)
+        counter += i
+    img = counter # png
+    sound = counter + 1 # wav
+    
+    return str(name + "test"), decisions, img, sound
+        
